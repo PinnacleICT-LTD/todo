d77590378c38a8b3df51e77ba47cb07e670e2b79
Yasin Tukur Muhammed <saukakke@gmail.com>
2021-05-26 05:49:59 -0700

    Added a text property

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Image, View } from 'react-native';
+import {Text, Image, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Image 
-        source={"../assets/mobile.svg"}
+        source={require('./assets/mobile.svg')}
       />
-      <StatusBar style="auto" />
+      <Text>Get things done with TODO</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
