176ed4c141c4de06e2ac33d30121ad2dcdf4e8f4
Yasin Tukur Muhammed <saukakke@gmail.com>
2021-05-26 05:24:30 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image 
+        source={"../assets/mobile.svg"}
+      />
       <StatusBar style="auto" />
     </View>
   );
